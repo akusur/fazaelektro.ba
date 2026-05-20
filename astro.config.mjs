a48c4fbdb7b8590281e5b105a2dd5ff1200e32fd
akusur <kusurbih@gmail.com>
2026-05-21 01:06:50 +0200

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://fazaelektro.ba',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
